870964ec6bb836cf5fee564ce87d734337667097
kyungeunni <kyungeunni@gmail.com>
2023-07-10 16:20:21 +0900

    chore: add license and parse flags

--- cmd/dt/main.go
+++ cmd/dt/main.go
@@ -1,3 +1,20 @@
+// Licensed to Elasticsearch B.V. under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Elasticsearch B.V. licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package main
 
 import (
@@ -20,6 +37,8 @@ func main() {
 	var cfg config
 
 	flag.Float64Var(&cfg.sampleRate, "sample-rate", 1.0, "set the sample rate. allowed value: min: 0.1, max: 1.0")
+	flag.Parse()
+
 	if err := Main(context.Background(), cfg); err != nil {
 		log.Fatal(err)
 	}
@@ -27,7 +46,7 @@ func main() {
 
 func Main(ctx context.Context, cfg config) error {
 	if cfg.sampleRate < 0.1 || cfg.sampleRate > 1.0 {
-		return errors.New("Invalid sample rate provided. allowed value: 0.1 <= sample_rate <= 1.0")
+		return errors.New("Invalid sample rate provided. allowed value: 0.1 <= sample-rate <= 1.0")
 	}
 
 	_, err := indexIntakeV2Trace(cfg, 0)
