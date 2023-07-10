a62365ee210649fe7b7e693a804bd2002f5154f1
kyungeunni <kyungeunni@gmail.com>
2023-07-10 16:00:08 +0900

    feat: add command sending v2 intake events using agent

---
+++ cmd/dt/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"fmt"
+	"log"
+	"math/rand"
+	"time"
+
+	"go.elastic.co/apm/v2"
+)
+
+type config struct {
+	sampleRate float64
+}
+
+func main() {
+	var cfg config
+
+	flag.Float64Var(&cfg.sampleRate, "sample-rate", 1.0, "set the sample rate. allowed value: min: 0.1, max: 1.0")
+	if err := Main(context.Background(), cfg); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Main(ctx context.Context, cfg config) error {
+	if cfg.sampleRate < 0.1 || cfg.sampleRate > 1.0 {
+		return errors.New("Invalid sample rate provided. allowed value: 0.1 <= sample_rate <= 1.0")
+	}
+
+	_, err := indexIntakeV2Trace(cfg, 0)
+
+	return err
+}
+
+func indexIntakeV2Trace(cfg config, idOverride byte) (byte, error) {
+	uniqueName := suffixString("trace")
+	serviceName := "service-" + uniqueName
+	tracer, err := apm.NewTracer(serviceName, "0.0.1")
+
+	if err != nil {
+		return 0, errors.New("Failed to instantiate tracer")
+	}
+	// set sample rate
+	ts := apm.NewTraceState(apm.TraceStateEntry{
+		Key: "es", Value: fmt.Sprintf("s:%.4g", cfg.sampleRate),
+	})
+	traceID := byte((rand.Float64() + 0.1) * 999)
+	if idOverride != 0 {
+		traceID = idOverride
+	}
+	traceContext := apm.TraceContext{
+		Trace:   apm.TraceID{traceID},
+		Options: apm.TraceOptions(0).WithRecorded(true),
+		State:   ts,
+	}
+
+	tx := tracer.StartTransactionOptions("parent-tx", "apmtool", apm.TransactionOptions{
+		TraceContext: traceContext,
+	})
+
+	span := tx.StartSpanOptions("parent-span", "apmtool", apm.SpanOptions{
+		Parent: tx.TraceContext(),
+	})
+
+	exit := tx.StartSpanOptions("exit-span", "apmtool", apm.SpanOptions{
+		Parent:   span.TraceContext(),
+		ExitSpan: true,
+	})
+
+	exit.Context.SetServiceTarget(apm.ServiceTargetSpanContext{
+		Type: "service_type",
+		Name: "service_name",
+	})
+
+	exit.Duration = 999 * time.Millisecond
+	exit.Outcome = "failure"
+
+	// error
+	e := tracer.NewError(errors.New("timeout"))
+	e.Culprit = "timeout"
+	e.SetSpan(exit)
+	e.Send()
+	exit.End()
+
+	span.Duration = time.Second
+	span.Outcome = "success"
+	span.End()
+	tx.Duration = 2 * time.Second
+	tx.Outcome = "success"
+	tx.End()
+	tracer.Flush(nil)
+
+	return traceID, nil
+}
+
+func suffixString(s string) string {
+	const letter = "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, 6)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return fmt.Sprintf("%s-%s", s, string(b))
+}
--- go.mod
+++ go.mod
@@ -7,15 +7,32 @@ require (
 	github.com/gofrs/flock v0.8.1
 	github.com/tidwall/gjson v1.14.4
 	github.com/urfave/cli/v3 v3.0.0-alpha4
+	go.elastic.co/apm v1.15.0
+	go.elastic.co/apm/v2 v2.4.3
 )
 
 require (
+	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
 	github.com/elastic/elastic-transport-go/v8 v8.3.0 // indirect
+	github.com/elastic/go-licenser v0.3.1 // indirect
+	github.com/elastic/go-sysinfo v1.7.1 // indirect
+	github.com/elastic/go-windows v1.0.0 // indirect
+	github.com/jcchavezs/porto v0.1.0 // indirect
+	github.com/joeshaw/multierror v0.0.0-20140124173710-69b34d4ec901 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/procfs v0.0.0-20190425082905-87a4384529e0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/santhosh-tekuri/jsonschema v1.2.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	go.elastic.co/fastjson v1.1.0 // indirect
+	golang.org/x/lint v0.0.0-20201208152925-83fdc39ff7b5 // indirect
+	golang.org/x/mod v0.3.0 // indirect
 	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/tools v0.0.0-20200509030707-2212a7e161a5 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+	howett.net/plist v0.0.0-20181124034731-591f970eefbb // indirect
 )
